ffa4b70b84a2e4a7258fc347b8ba0710adec4770
Atee134 <l.atee.1995@gmail.com>
2018-10-04 19:33:20 +0200

    added nswag for dto generation

--- AG-SPA/src/app/app.module.ts
+++ AG-SPA/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { appRoutes } from "./routes";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes),
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ AG-SPA/src/app/routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const appRoutes: Routes = [
+//   { path: '', component: HomeComponent }, 
+//   {
+//       path: '',
+//       runGuardsAndResolvers: 'always',
+//       canActivate: [AuthGuard],
+//       children: [
+//         { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver} },
+//         { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver} },
+//         { path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges] },
+//         { path: 'messages', component: MessagesComponent },
+//         { path: 'lists', component: ListsComponent },
+//       ]
+//   },
+//   { path: '**', redirectTo: '', pathMatch: 'full' },
+];
